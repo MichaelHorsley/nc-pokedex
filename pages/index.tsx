e0eb38f498c6bdc2d0be57c4e89a38a3015a454f
Michael Horsley <omega_mike@hotmail.com>
2022-05-30 20:56:39 +0100

    Some cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, GridItem } from '@chakra-ui/react'
+import { Box, Center, Grid, GridItem, Text } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Image from 'next/image';
 import { Fragment } from 'react';
@@ -8,28 +8,32 @@ const Home: NextPage = ({data}) => {
   console.log(data);
 
   return (
-    <Box padding="100px" bg="gray.900" height="100vh" width="100vw" color="white">
+    <Box padding="100px" bg="gray.900" width="100vw" color="white">
       <Grid templateColumns="repeat(36, 1fr)" templateRows="repeat(2, 1fr)" gap={1}>
-        <GridItem colSpan={6} rowSpan={2} bg="#2b4f01" borderRadius="5px" textAlign="center">No.</GridItem>
-        <GridItem colSpan={6} rowSpan={2} bg="#2b4f01" borderRadius="5px" textAlign="center">Pic</GridItem>
-        <GridItem colSpan={6} rowSpan={2} bg="#2b4f01" borderRadius="5px" textAlign="center">Name</GridItem>
-        <GridItem colSpan={6} rowSpan={2} bg="#2b4f01" borderRadius="5px" textAlign="center">Type</GridItem>
-        <GridItem colSpan={6} rowSpan={2} bg="#2b4f01" borderRadius="5px" textAlign="center">Abilities</GridItem>
-        <GridItem colSpan={6} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center">Base Stats</GridItem>
-        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center">HP</GridItem>
-        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center">Att</GridItem>
-        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center">Def</GridItem>
-        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center">S.Att</GridItem>
-        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center">S.Def</GridItem>
-        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center">Spd</GridItem>
+        <GridItem colSpan={6} rowSpan={2} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">No.</Center></GridItem>
+        <GridItem colSpan={6} rowSpan={2} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">Pic</Center></GridItem>
+        <GridItem colSpan={6} rowSpan={2} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">Name</Center></GridItem>
+        <GridItem colSpan={6} rowSpan={2} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">Type</Center></GridItem>
+        <GridItem colSpan={6} rowSpan={2} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">Abilities</Center></GridItem>
+        <GridItem colSpan={6} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">Base Stats</Center></GridItem>
+        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">HP</Center></GridItem>
+        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">Att</Center></GridItem>
+        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">Def</Center></GridItem>
+        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">S.Att</Center></GridItem>
+        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">S.Def</Center></GridItem>
+        <GridItem colSpan={1} rowSpan={1} bg="#2b4f01" borderRadius="5px" textAlign="center"><Center height="100%">Spd</Center></GridItem>
 
         {data.results.map((pokemon, i) => (
           <Fragment key={pokemon.name}>
-            <GridItem colSpan={6} rowSpan={1} bg="gray.800" borderRadius="5px" textAlign="center">#{i+1}</GridItem>
+            <GridItem colSpan={6} rowSpan={1} bg="gray.800" borderRadius="5px">
+              <Center height="100%">#{i+1}</Center>
+            </GridItem>
             <GridItem colSpan={6} rowSpan={1} bg="gray.800" borderRadius="5px" textAlign="center">
               <Image width="100px" height="100px" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png`}/>
             </GridItem>
-            <GridItem colSpan={6} rowSpan={1} bg="gray.800" borderRadius="5px" textAlign="center">{pokemon.name}</GridItem>
+            <GridItem colSpan={6} rowSpan={1} bg="gray.800" borderRadius="5px" textAlign="center">
+              <Center height="100%">{pokemon.name}</Center>
+            </GridItem>
             <GridItem colSpan={6} rowSpan={1} bg="gray.800" borderRadius="5px" textAlign="center"></GridItem>
             <GridItem colSpan={6} rowSpan={1} bg="gray.800" borderRadius="5px" textAlign="center"></GridItem>
             <GridItem colSpan={6} rowSpan={1} bg="gray.800" borderRadius="5px" textAlign="center"></GridItem>
